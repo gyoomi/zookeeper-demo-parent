fdf24b3e84c091ae8388bfebf30c275416edf453
gyoomi <gyoomi0709@foxmail.com>
2020-07-16 14:38:04 +0800

    zk crud

--- the-starter-of-zk/src/main/java/ZkTest01.java
+++ the-starter-of-zk/src/main/java/ZkTest01.java
@@ -4,7 +4,7 @@
  * All Rights Reserved.
  */
 
-import org.apache.zookeeper.*;
+import org.apache.zookeeper.ZooKeeper;
 
 /**
  * zk Test
@@ -15,15 +15,35 @@ import org.apache.zookeeper.*;
 public class ZkTest01
 {
 
-	public static final String CONNECT_STRING = "192.168.200.11:2181";
+	public static final String CONNECT_STRING = "192.168.200.11:2181,192.168.200.12:2181,192.168.200.13:2181";
 
-	public static final int TIMEOUT = 10000;
+	public static final int TIMEOUT = 20000;
 
 	public static void main(String[] args) throws Exception
 	{
 		ZooKeeper zk = new ZooKeeper(CONNECT_STRING, TIMEOUT, watchedEvent -> System.out.println("Watching: " + watchedEvent.getType() + "  " + watchedEvent.getPath()));
 
-		String result = zk.create("/idea", "idea zk".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-		System.out.println(result);
+		// 1. 创建：数据节点到zk中
+//		String result = zk.create("/idea", "idea zk".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+//		System.out.println(result);
+
+		// 2. 判断： 判断znode节点是否存在
+//		Stat exists = zk.exists("/idea", false);
+//		System.out.println(exists);
+
+		// 3. 获取： 获取子节点
+//		List<String> children = zk.getChildren("/", true);
+//		System.out.println(children);
+//		Thread.sleep(Long.MAX_VALUE);
+
+		// 4. 获取： 获取节点的数据
+//		byte[] data = zk.getData("/idea", true, null);
+//		System.out.println(new String(data));
+
+		// 5. 删除： 删除znode节点
+		// 参数2：指定要删除的版本，-1表示删除所有版本
+//		zk.delete("/idea", -1);
+
+		// 6.
 	}
 }
